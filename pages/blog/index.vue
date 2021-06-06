86018335b8566e76d7888ebfffd3e7289525936e
Shivelle <shivelle@gmx.net>
2021-06-06 14:02:16 +0200

    refactor blog index pages

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="blog container mx-auto">
-    <section class="grid grid-cols-3 gap-4 pt-12">
-      <article
+  <section class="section blog">
+    <div class="columns is-mobile">
+      <card
         v-for="(post, $index) in posts"
         :key="`post-${$index}`"
         class=""
+        :title="post.title"
+        :image="post.media"
+        :description="post.description"
       >
-        <img class="w-full" :src="post.media" :alt="post.title">
-        <div class="px-6 py-4 flex-2">
-          <h3>{{ post.title }}</h3>
-          <p class="text-gray-700 text-base">
-            {{ post.description }}
-          </p>
-        </div>
-        <footer class="p-4">
-          <nuxt-link :to="post.path" class="font-bold text-xl mb-2">
-            <button :to="post.path" class="btn btn-teal">
-              {{ $t('read-more') }}
-            </button>
-          </nuxt-link>
-        </footer>
-      </article>
-    </section>
-  </div>
+      <footer class="p-2">
+        <nuxt-link :to="post.path" class="font-bold text-xl mb-2">
+          <button :to="post.path" class="button is-primary is-light">
+            {{ $t('read-more') }}
+          </button>
+        </nuxt-link>
+      </footer>
+      </card>
+    </div>
+  </section>
 </template>
 
 <script>
+import Card from '~/components/Card'
+
 export default {
   name: 'Blog',
+  components: {
+    Card
+  },
   async asyncData (context) {
     const { $content } = context
     const posts = await $content(context.route.path).fetch()
