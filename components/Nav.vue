d9e77347af0c38ffca8c989890e80fb6721b045a
Shivelle <shivelle@gmx.net>
2021-06-09 06:21:52 +0200

    use I18n-localized nuxt-links to fix broken links and change colors

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav class="navbar has-background-black-ter" role="navigation" aria-label="main navigation">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="/">
+      <nuxt-link
+      :to="localePath('/')"
+      class="navbar-item">
         <img src="~assets/img/svg/bird.svg" width="112" height="28">
-      </a>
+      </nuxt-link>
 
       <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="mainNav">
         <span aria-hidden="true" />
@@ -14,12 +16,13 @@
 
     <div id="mainNav" class="navbar-end">
       <div class="navbar-start">
-        <a class="navbar-item has-text-light">
-          Portfolio
-        </a>
-
-        <a class="navbar-item has-text-light" href="/blog">
+        <nuxt-link
+        :to="localePath('blog')"
+        class="navbar-item">
           Blog
+        </nuxt-link>
+        <a class="navbar-item">
+          Kontakt
         </a>
       </div>
     </div>
