5764f9246d4f3d1db79d91a5705943a057f51e44
Shivelle <shivelle@gmx.net>
2021-06-09 05:47:35 +0200

    use component for nav

---
+++ components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar has-background-black-ter" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="~assets/img/svg/bird.svg" width="112" height="28">
+      </a>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="mainNav">
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+
+    <div id="mainNav" class="navbar-end">
+      <div class="navbar-start">
+        <a class="navbar-item has-text-light">
+          Portfolio
+        </a>
+
+        <a class="navbar-item has-text-light" href="/blog">
+          Blog
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
