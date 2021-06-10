5d5c49c77df045a4577fb9dfcb58087f792718c8
Shivelle <shivelle@gmx.net>
2021-06-10 06:32:44 +0200

    begin hero section

---
+++ components/Hero.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-img"></div>
+    <div class="hero-body">
+      <div class="container">
+        <h4 class="subtitle has-text-white mb-5">
+          Willkommen <br>
+          in meinem virtuellen Zuhause
+        </h4>
+        <h1 class="title has-text-white is-size-1 is-uppercase has-text-weight-bold mt-5">
+          <span class="has-highlight-color">HALLO</span> ICH BIN <br>SARAH.
+        </h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .hero-img {
+    background-image: url('~/assets/img/hero-img.jpg');
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    filter: blur(8px);
+    -webkit-filter: blur(8px);
+    transform: scale(1.1);
+  }
+
+  .hero-body {
+    z-index: 2;
+  }
+
+  .has-highlight-color {
+    color: #ffa726;
+  }
+</style>
