d305752e687d3ad2f514ab62669ac1b1291a37e1
Shivelle <shivelle@gmx.net>
2021-06-10 21:50:16 +0200

    add and buetify form

---
+++ components/Contact.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="section">
+    <h3 class="is-size-2">Kontakt</h3>
+    <form
+    name="portfolio"
+    class="js-form"
+    novalidate="novalidate"
+    netlify-honeypot="bot-field"
+    method="POST"
+    data-netlify-recaptcha="true"
+    netlify>
+      <div class="row">
+        <p class="is-hidden">
+          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
+        <b-field label="Name" :label-position="labelPosition">
+          <b-input type="text" name="name" required="" placeholder="Name">
+          </b-input>
+        </b-field>
+        <b-field label="E-Mail">
+          <b-input type="email" name="email" placeholder="E-Mail">
+          </b-input>
+        </b-field>
+        <b-field class="field">
+          <b-input type="text" name="subject" placeholder="Betreff (optinal)"></b-input>
+        </b-field>
+        <b-field class="field">
+          <b-input
+           type="textarea"
+           name="message"
+           required=""
+           placeholder="Nachricht*"
+           aria-required="true"></b-input>
+          <div data-netlify-recaptcha="true"></div>
+        </b-field>
+        <b-field>
+          <p class="control">
+            <b-button
+            class="button is-primary"
+            type="submit"
+            value="Send"
+            >Nachricht senden</b-button>
+          </p>
+        </b-field>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      labelPosition: 'on-border'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .field.label::before {
+    color: #ffa726;
+  }
+</style>
