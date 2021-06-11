fc6a193df621bfd362cf63be9994bba910b09137
Shivelle <shivelle@gmx.net>
2021-06-11 06:33:44 +0200

    add global styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,8 +22,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+  // global styling
+  $dark: #272727;
+  $orange: #ffa726;
+
   .bg-dark {
-    background-color: #272727;
+    background-color: $dark;
+  }
+
+  .bg-dark-light {
+    background-color: rgba($dark, 0.5);
+  }
+
+  .bg-orange {
+    background-color: $orange;
+  }
+
+  .txt-orange {
+    color: $orange;
   }
 </style>
