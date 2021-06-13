9a9d61bf95a3bfa121dec2e55ae35f2772025a26
Shivelle <shivelle@gmx.net>
2021-06-13 17:45:01 +0200

    use dark bg for footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer">
+  <footer class="footer bg-dark">
     <div class="content has-text-centered">
       <p>
         Made with ❤️ by <a href="https://github.com/Shivelle" target="_blank">Sarah Maria Leidisch</a>.
