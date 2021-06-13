509b903f767c7c15445c2f0343b1d3c859fd18a1
Shivelle <shivelle@gmx.net>
2021-06-13 11:47:39 +0200

    add progress bars to skills section

--- components/Skills.vue
+++ components/Skills.vue
@@ -4,6 +4,19 @@
       Ich gestalte & entwickle ansprechende Nutzererlebnisse
     </h2>
     <p>Mit vier Jahren Erfahrung in der Entwicklung anspruchsvoller WebApps und Landingpages.</p>
-    <!-- TODO: Skill Bars -->
+    <div class="mt-6">
+      <h4 class="is-size-6">
+        Frontend Entwicklung
+      </h4>
+      <b-progress size="is-small mb-2" :value="70" />
+      <h4 class="is-size-6">
+        Backend Entwicklung
+      </h4>
+      <b-progress size="is-small mb-2" :value="60" />
+      <h4 class="is-size-6">
+        Design
+      </h4>
+      <b-progress size="is-small mb-2" :value="70" />
+    </div>
   </section>
 </template>
