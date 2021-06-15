f3316548045812b3c39a9aeed4754f679f05e6e4
Shivelle <shivelle@gmx.net>
2021-06-15 06:39:22 +0200

    wip: project grid

--- components/Projects.vue
+++ components/Projects.vue
@@ -4,8 +4,64 @@
       <h2 class="is-size-2 is-uppercase">
         Projekte
       </h2>
-      <p>An diesen Projekten war ich alleine, aber auch als Teil eines Teams, beteiligt.</p>
-      <!-- TODO: Project Grid -->
+      <p class="has-text-grey-light">
+        An diesen Projekten war ich alleine, aber auch als Teil eines Teams, beteiligt.
+      </p>
+      <!-- TODO: Update Project Grid -->
+      <div class="tile is-ancestor is-vertical">
+        <div class="tile is-parent">
+          <div class="tile is-child p-4">
+            <a href="https://echtpost.de/" target="_blank">
+              <div class="">
+                <h3 class="has-text-white is-size-3">Postkartenversand</h3>
+                <div class="has-text-grey-light">Landingpage Redesign</div>
+              </div>
+            </a>
+          </div>
+          <div class="tile is-child p-4">
+            <a href="https://centralstationcrm.de/" target="_blank">
+              <div class="">
+                <h3 class="has-text-white is-size-3">CRM</h3>
+                <div class="has-text-grey-light">Redesign Backend</div>
+                <div class="has-text-grey-light">Diverse Landingpages</div>
+                <div class="has-text-grey-light">Custom Captcha Feature</div>
+              </div>
+            </a>
+          </div>
+          <div class="tile is-child p-4">
+            <a href="https://www.hn-feinmahltechnik.de/" target="_blank">
+              <div class="">
+                <h3 class="has-text-white is-size-3">Vermahlungsdienstleister</h3>
+                <div class="has-text-grey-light">Homepage-Design</div>
+                <div class="has-text-grey-light">Visitenkarten-Design</div>
+                <div class="has-text-grey-light">WordPress-Template-Entwicklung</div>
+                <div class="has-text-grey-light">Hosting- & E-Mail-Setup</div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="tile is-parent">
+          <div class="tile is-child p-4">
+            <a href="https://centralplanner.de/" target="_blank">
+              <div class="">
+                <h3 class="has-text-white is-size-3">Buchungssystem</h3>
+                <div class="has-text-grey-light">Redesign Backend</div>
+                <div class="has-text-grey-light">Auto-Mailing Features</div>
+                <div class="has-text-grey-light">Custom Captcha Feature</div>
+              </div>
+            </a>
+          </div>
+          <div class="tile is-child p-4">
+            <a href="https://centraldesk.de/" target="_blank">
+              <div class="">
+                <h3 class="has-text-white is-size-3">Supportsystem</h3>
+                <div class="has-text-grey-light">Mehrsprachige Signaturen</div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
