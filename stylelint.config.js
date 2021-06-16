8dada6f66be8f05dd8b857f26ba6421904a00519
Shivelle <shivelle@gmx.net>
2021-06-16 21:18:58 +0200

    update stylelint scss configs

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,12 @@ module.exports = {
   extends: [
     'stylelint-config-standard'
   ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
+  plugins: [
+    'stylelint-scss'
+  ],
+  rules: {
+    // recommended rules
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true
+  }
 }
