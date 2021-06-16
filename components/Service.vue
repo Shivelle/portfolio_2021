a205190001855bd8989dd133bcf91be4641f9832
Shivelle <shivelle@gmx.net>
2021-06-16 06:34:25 +0200

    add margin to title

--- components/Service.vue
+++ components/Service.vue
@@ -2,7 +2,7 @@
   <section class="section has-text-white is-fullheight">
     <div class="container">
       <p>Mein Service</p>
-      <h2 class="is-size-2 is-uppercase">
+      <h2 class="is-size-2 mb-6 mt-3 is-uppercase">
         Ihr <span class="txt-highlight">nächstes Projekt</span> ist meine Inspiration
       </h2>
       <!-- TODO: Finish list and 4 years section -->
