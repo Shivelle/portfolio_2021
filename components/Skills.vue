f76b4dbe823932ca8f05ad501623d04da6c62fa9
Shivelle <shivelle@gmx.net>
2021-06-17 06:42:37 +0200

    refactor progress bar flex alignment

--- components/Skills.vue
+++ components/Skills.vue
@@ -10,34 +10,42 @@
           <p class="has-text-grey-light">Mit vier Jahren Erfahrung in der Entwicklung anspruchsvoller WebApps und Landingpages.</p>
         </div>
 
-        <div class="column has-text-grey-light">
-          <div class="is-clearfix p-1">
-            <div class="is-size-6 is-pulled-left">
-              Frontend Entwicklung
+        <div class="column has-text-grey-light is-flex is-flex-direction-column is-justify-content-space-evenly">
+          <div>
+            <div class="is-clearfix p-1">
+              <div class="is-size-6 is-pulled-left">
+                Frontend Entwicklung
+              </div>
+              <div class="is-size-6 is-pulled-right">
+                70%
+              </div>
             </div>
-            <div class="is-size-6 is-pulled-right">
-              70%
-            </div>
+            <b-progress size="is-small mb-4" :value="70" type="is-primary" />
           </div>
-          <b-progress size="is-small mb-4" :value="70" type="is-primary" />
-          <div class="is-clearfix p-1">
-            <div class="is-size-6 is-pulled-left">
-              Backend Entwicklung
+
+          <div>
+            <div class="is-clearfix p-1">
+              <div class="is-size-6 is-pulled-left">
+                Backend Entwicklung
+              </div>
+              <div class="is-size-6 is-pulled-right">
+                60%
+              </div>
             </div>
-            <div class="is-size-6 is-pulled-right">
-              60%
-            </div>
+            <b-progress size="is-small mb-4" :value="60" type="is-primary" />
           </div>
-          <b-progress size="is-small mb-4" :value="60" type="is-primary" />
-          <div class="is-clearfix p-1">
-            <div class="is-size-6 is-pulled-left">
-              Design
+
+          <div>
+            <div class="is-clearfix p-1">
+              <div class="is-size-6 is-pulled-left">
+                Design
+              </div>
+              <div class="is-size-6 is-pulled-right">
+                70%
+              </div>
             </div>
-            <div class="is-size-6 is-pulled-right">
-              70%
-            </div>
+            <b-progress size="is-small mb-4" :value="70" type="is-primary" />
           </div>
-          <b-progress size="is-small mb-4" :value="70" type="is-primary" />
         </div>
       </div>
     </div>
