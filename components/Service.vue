58d7d25d3b6ef4aa40944e618045bfa4d5e5c78d
Shivelle <shivelle@gmx.net>
2021-06-17 06:42:12 +0200

    add to service section content

--- components/Service.vue
+++ components/Service.vue
@@ -1,15 +1,31 @@
 <template>
   <section class="section has-text-white is-fullheight">
     <div class="container">
-      <p>Mein Service</p>
-      <h2 class="is-size-2 mb-6 mt-3 is-uppercase">
-        Ihr <span class="txt-highlight">nächstes Projekt</span> ist meine Inspiration
-      </h2>
-      <!-- TODO: Finish list and 4 years section -->
-      <ul>
-        <li>Entwicklung</li>
-        <li>Design</li>
-      </ul>
+      <div class="columns">
+        <div class="column">
+          <p>Mein Service</p>
+          <h2 class="is-size-2 mb-6 mt-3 is-uppercase">
+            Ihr <span class="txt-highlight">nächstes Projekt</span> ist meine Inspiration
+          </h2>
+          <!-- TODO: Finish list and 4 years section -->
+          <ul>
+            <li>Entwicklung</li>
+            <li>Design</li>
+          </ul>
+        </div>
+        <div class="column">
+          <div class="dots-image-2">
+            <img alt="" class="img-responsive" src="images/m2.jpg">
+            <div class="dots"></div>
+            <h3 class="is-size-4">
+              <div class="is-primary">
+                4
+              </div>
+              Jahre<br>Erfahrung<br>als Freelancer
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
