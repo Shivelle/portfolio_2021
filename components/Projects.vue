71feea143aa73e3f7ea50f1b7316a4a80fc7839a
Shivelle <shivelle@gmx.net>
2021-06-17 06:41:56 +0200

    refactor project grid

--- components/Projects.vue
+++ components/Projects.vue
@@ -8,61 +8,61 @@
         An diesen Projekten war ich alleine, aber auch als Teil eines Teams, beteiligt.
       </p>
       <!-- TODO: Update Project Grid -->
-      <div class="tile is-ancestor is-vertical mt-6">
+      <div class="is-flex is-flex-wrap-wrap mt-6">
         <div class="tile is-parent">
-          <article class="tile is-child p-4 bg-dark">
+          <article class="tile is-child p-4 bg-ep">
             <a href="https://echtpost.de/" target="_blank">
-              <div class="">
-                <h3 class="has-text-white is-size-3">Postkartenversand</h3>
-                <div class="has-text-grey-light">Landingpage Redesign</div>
+              <div class="has-text-white">
+                <h3 class="is-size-3 has-text-weight-semibold mb-3">Postkartenversand</h3>
+                <div class="">Landingpage Redesign</div>
               </div>
             </a>
           </article>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child p-4 bg-dark">
+          <article class="tile is-child p-4 bg-crm">
             <a href="https://centralstationcrm.de/" target="_blank">
-              <div class="">
-                <h3 class="has-text-white is-size-3">CRM</h3>
-                <div class="has-text-grey-light">Redesign Backend</div>
-                <div class="has-text-grey-light">Diverse Landingpages</div>
-                <div class="has-text-grey-light">Custom Captcha Feature</div>
+              <div class="has-text-white">
+                <h3 class="is-size-3 has-text-weight-semibold mb-3">CRM</h3>
+                <div class="">Redesign Backend</div>
+                <div class="">Diverse Landingpages</div>
+                <div class="">Custom Captcha Feature</div>
               </div>
             </a>
           </article>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child p-4 bg-dark">
+          <article class="tile is-child p-4 bg-hn">
             <a href="https://www.hn-feinmahltechnik.de/" target="_blank">
-              <div class="">
-                <h3 class="has-text-white is-size-3">Vermahlungsdienstleister</h3>
-                <div class="has-text-grey-light">Homepage-Design</div>
-                <div class="has-text-grey-light">Visitenkarten-Design</div>
-                <div class="has-text-grey-light">WordPress-Template-Entwicklung</div>
-                <div class="has-text-grey-light">Hosting- & E-Mail-Setup</div>
+              <div class="has-text-white">
+                <h3 class="is-size-3 has-text-weight-semibold mb-3">Vermahlungsdienstleister</h3>
+                <div class="">Homepage-Design</div>
+                <div class="">Visitenkarten-Design</div>
+                <div class="">WordPress-Template-Entwicklung</div>
+                <div class="">Hosting- & E-Mail-Setup</div>
               </div>
             </a>
           </article>
         </div>
 
         <div class="tile is-parent">
-          <article class="tile is-child p-4 bg-dark">
+          <article class="tile is-child p-4 bg-cp">
             <a href="https://centralplanner.de/" target="_blank">
-              <div class="">
-                <h3 class="has-text-white is-size-3">Buchungssystem</h3>
-                <div class="has-text-grey-light">Redesign Backend</div>
-                <div class="has-text-grey-light">Auto-Mailing Features</div>
-                <div class="has-text-grey-light">Custom Captcha Feature</div>
+              <div class="has-text-white">
+                <h3 class="is-size-3 has-text-weight-semibold mb-3">Buchungssystem</h3>
+                <div class="">Redesign Backend</div>
+                <div class="">Auto-Mailing Features</div>
+                <div class="">Custom Captcha Feature</div>
               </div>
             </a>
           </article>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child p-4 bg-dark">
+          <article class="tile is-child p-4 bg-cd">
             <a href="https://centraldesk.de/" target="_blank">
-              <div class="">
-                <h3 class="has-text-white is-size-3">Supportsystem</h3>
-                <div class="has-text-grey-light">Mehrsprachige Signaturen</div>
+              <div class="has-text-white">
+                <h3 class="is-size-3 has-text-weight-semibold mb-3">Supportsystem</h3>
+                <div class="">Mehrsprachige Signaturen</div>
               </div>
             </a>
           </article>
