2bf89792eb74b1cabaa679460c6d4f25e7babc20
Shivelle <shivelle@gmx.net>
2021-06-19 17:58:01 +0200

    finish carousel in code section

--- components/Code.vue
+++ components/Code.vue
@@ -1,34 +1,94 @@
 <template>
-  <section class="section has-text-white bg-dark-lighter">
+  <section class="section has-text-white bg-dark-lighter is-relative">
+    <div class="section-bg" />
     <div class="container">
       <div class="columns">
         <div class="column">
           <h2 class="is-size-2 is-uppercase mb-6">
-            Einblick: Code
+            Einblick: <span class="txt-highlight">
+              Code
+            </span>
           </h2>
           <p class="has-text-grey-light">
             Gerade in meinen Arbeitsfeldern gilt: Man lernt nie aus. Ich arbeite ständig an meiner Weiterbildung. Hier erhalten Sie Einblicke in mein GitHub und CodePen Portfolio, sowie einige meiner privaten Demos.
           </p>
         </div>
         <div class="column">
-          <b-carousel>
+          <b-carousel
+            :has-drag="true"
+          >
             <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-              <section :class="`hero is-medium is-${carousel.color}`">
-                <div class="hero-body has-text-left">
+              <div class="px-6 pt-4 carousel-content">
+                <div class="has-text-left">
                   <h3 class="title is-size-3 mb-3 has-text-white">
                     {{ carousel.title }}
                   </h3>
-                  <p class="has-text-grey-light">
+                  <p class="has-text-grey-light mb-6">
                     {{ carousel.description }}
                   </p>
-                  <a href="#" target="_blank">
+                  <a v-if="`${carousel.profileSrc}`" :href="`${carousel.profileSrc}`" target="_blank" class="is-primary">
+                    <span>
+                      <font-awesome-icon :icon="['fab', `${carousel.icon}`]" />
+                    </span>
                     Zum Profil
                   </a>
-                  <div class="snippet">
-                    {{ carousel.snippet }}
+                  <img v-if="`${carousel.imgSrc}`" :src="`${carousel.imgSrc}`" :alt="`${carousel.title}`" class="my-4" loading="lazy">
+                  <div v-if="`${carousel.pens}`" class="my-4 columns">
+                    <div class="column">
+                      <p>The Cat Blog</p>
+                      <a href="https://codepen.io/Shivelle/full/aKXVJw" target="_blank">
+                        <img src="~assets/img/cp/cat_blog.jpg" alt="cat blog" loading="lazy">
+                      </a>
+                    </div>
+                    <div class="column">
+                      <p>Circular Navigation</p>
+                      <a href="https://codepen.io/Shivelle/full/eBxEvJ" target="_blank">
+                        <img src="~assets/img/cp/circular_nav.jpg" alt="circular nav" loading="lazy">
+                      </a>
+                    </div>
+                    <div class="column">
+                      <p>Click and Drag</p>
+                      <a href="https://codepen.io/Shivelle/full/MvEpvb" target="_blank">
+                        <img src="~assets/img/cp/click_n_drag.jpg" alt="click & drag" loading="lazy">
+                      </a>
+                    </div>
+                  </div>
+                  <div v-if="`${carousel.pens}`" class="my-2 columns">
+                    <div class="column">
+                      <p>Infinite Gallery</p>
+                      <a href="https://codepen.io/Shivelle/full/vMqxMV" target="_blank">
+                        <img src="~assets/img/cp/infinite_scroll.jpg" alt="infinite scroll" loading="lazy">
+                      </a>
+                    </div>
+                    <div class="column">
+                      <p>Progress Bars</p>
+                      <a href="https://codepen.io/Shivelle/full/MvJzwm" target="_blank">
+                        <img src="~assets/img/cp/progress.jpg" alt="progress bars" loading="lazy">
+                      </a>
+                    </div>
+                    <div class="column">
+                      <p>Custom Video Player</p>
+                      <a href="https://codepen.io/Shivelle/full/vJBQeE" target="_blank">
+                        <img src="~assets/img/cp/video_player.jpg" alt="video player" loading="lazy">
+                      </a>
+                    </div>
+                  </div>
+                  <div v-if="`${carousel.demos}`" class="columns">
+                    <div class="column">
+                      <span>Flexbox Layout</span>
+                      <a href="https://hungry-poitras-6ae589.netlify.com" target="_blank">
+                        <img src="~assets/img/demos/mountain_travel.jpg" alt="Mountain Travel" loading="lazy">
+                      </a>
+                    </div>
+                    <div class="column">
+                      <span>Portfolio 2017</span>
+                      <a href="https://shivelle.github.io/" target="_blank">
+                        <img src="~assets/img/demos/hp_old.jpg" alt="Homepage V1" loading="lazy">
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </section>
+              </div>
             </b-carousel-item>
           </b-carousel>
         </div>
@@ -45,23 +105,51 @@ export default {
         {
           title: 'GitHub',
           description: 'GitHub ist ein Onlinedienst, der Software-Entwicklungsprojekte auf seinen Servern bereitstellt. Ich nutze sowohl Git via GitHub und GitLab, als auch Mercurial via BitBucket zur Versionsverwaltung.',
-          href: 'https://github.com/Shivelle',
-          snippet: '<img src="https://ghchart.rshah.org/FFA726/shivelle" alt="Github Chart" class="ghchart" />'
+          profileSrc: 'https://github.com/Shivelle',
+          imgSrc: 'https://ghchart.rshah.org/FFA726/shivelle',
+          icon: 'github',
+          pens: '',
+          demos: ''
         },
         {
           title: 'CodePen',
           description: 'CodePen ist eine Online-Community zum Testen und Zeigen von HTML-, CSS- und JavaScript-Codeschnipseln, die von Benutzern erstellt worden sind. Ich nutze den Online-Code-Editor seit vielen Jahren für Frontend-Snippets.',
-          href: 'https://codepen.io/shivelle',
-          snippet: ''
+          profileSrc: 'https://codepen.io/shivelle',
+          icon: 'codepen',
+          imgSrc: '',
+          pens: true,
+          demos: ''
         },
         {
           title: 'Demos',
           description: 'Eine kleine Auswahl meiner übrigen Projekte. Mein altes Portfolio aus dem Jahr 2017 und ein Flexbox Layout.',
-          href: 'https://github.com/Shivelle',
-          snippet: ''
+          imgSrc: '',
+          profileSrc: '',
+          pens: '',
+          demos: true
         }
       ]
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .section-bg {
+    background-image: url('~/assets/img/bg/abstract2.jpg');
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    opacity: 0.2;
+
+    @include prefix('filter', 'blur(4px)');
+  }
+
+  .carousel-content {
+    height: 80vh;
+  }
+</style>
