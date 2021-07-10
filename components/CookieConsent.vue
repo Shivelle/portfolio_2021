c23c5c2a376d2fc585d03ea840dcf701334c517b
Shivelle <shivelle@gmx.net>
2021-07-10 17:57:22 +0200

    consent info banner - first draft

---
+++ components/CookieConsent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cookie-consent bg-dark-lighter has-text-white has-text-centered p-4">
+    Auf dieser Webseite werden nur technisch relevante Cookies verwendet.
+    <span class="interactions pl-4">
+      <nuxt-link
+        :to="localePath('data-protection')"
+      >
+        {{ $t('data-protection') }}
+      </nuxt-link>
+      <button id="close-banner" class="button is-primary is-light">
+        Cool!
+      </button>
+    </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .cookie-consent {
+    position: fixed;
+    content: '';
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+</style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -22,6 +22,7 @@
         </p>
       </div>
     </footer>
+    <CookieConsent />
   </section>
 </template>
 
