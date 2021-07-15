96b206bd94fc54838e17048c3e9577750588df94
Shivelle <shivelle@gmx.net>
2021-07-15 21:59:33 +0200

    add simple thank you page to veryfiy form action

---
+++ pages/thank-you.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="content">
+    <Header />
+    <main>
+      <div class="px-2 my-2 container">
+        <p class="is-size-4 has-text-white mt-6">
+          {{ $t('contactForm.confirm') }}
+        </p>
+      </div>
+    </main>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .content {
+    min-height: 80vh;
+  }
+</style>
